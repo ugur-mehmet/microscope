7957383f674fcca3c323a3f2807db91d6126481d
ugur-mehmet <ugur_mehmet@yahoo.com>
2015-01-16 10:19:59 -0500

    Updated

---
+++ server/fixtures.js
@@ -0,0 +1,18 @@
+if (Posts.find().count()===0)
+	{
+		Posts.insert({
+			title: 'Introducing Telescope',
+			url: 'http://sachagreif.com/introducing-telescope'
+		});
+
+		Posts.insert({
+			title: 'Meteor',
+			url: 'http://meteor.com'
+		});
+
+		Posts.insert({
+			title: 'The meteor Book',
+			url: 'http://themeteorbook.com'
+		});
+
+	}
