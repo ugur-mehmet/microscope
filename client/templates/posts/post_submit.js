ce15a64b8aaa131a95674c6e163c49b231efc8e9
ugur-mehmet <ugur_mehmet@yahoo.com>
2015-01-26 07:28:37 -0500

    Updated

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,8 +7,17 @@ Template.postSubmit.events({
 			title: $(e.target).find('[name=title]').val() 
 		};
 
-		post._id = Posts.insert(post);
-		Router.go('postPage',post);
+		Meteor.call('postInsert', post, function(error,result) {
+			if(error)
+				return alert(error.reason);
+			//post._id = Posts.insert(post);
+
+			if (result.postExists)
+				alert('This link has already been posted');
+			
+			Router.go('postPage',{_id: result._id});
+		});
+		
 
 	}
 });
